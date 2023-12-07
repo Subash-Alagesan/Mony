3e206ba90cc2eff1984a41014c9d3f28608a32db
Subash-Alagesan <subashvgp@gmail.com>
2023-12-07 10:59:53 +0530

    Login Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from "react";
+import * as Components from './Form/Components';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [signIn, toggle] = React.useState(true);
+     return(
+        
+         <Components.Container>
+             
+             <Components.SignInContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Sign in</Components.Title>
+                      <Components.Input type='email' placeholder='Email' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                      <Components.Button>Sigin In</Components.Button>
+                  </Components.Form>
+             </Components.SignInContainer>
+
+             <Components.OverlayContainer signinIn={signIn}>
+                 <Components.Overlay signinIn={signIn}>
+
+              
+
+                     <Components.RightOverlayPanel signinIn={signIn}>
+                       <Components.Title1>Welcome to login</Components.Title1>
+                       <Components.Paragraph>
+                           Don't have an account?
+                       </Components.Paragraph>
+                           <Components.GhostButton1 onClick={() => toggle(true)}>
+                               SignUp
+                           </Components.GhostButton1> 
+                     </Components.RightOverlayPanel>
+ 
+                 </Components.Overlay>
+             </Components.OverlayContainer>
+
+         </Components.Container>
+     )
 }
 
 export default App;
