332dd22447eb36781b91c606621f599a06209895
Revathi <revathibecsc@gmail.com>
2024-02-03 10:48:59 +0530

    ...

--- src/Dashboard/AddProduct.js
+++ src/Dashboard/AddProduct.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
-import './AddProduct.css';
+import React, { useState } from "react";
+import "./AddProduct.css";
 import Grid from "@mui/material/Grid";
 
 const AddProduct = () => {
   // State variables for input values
-  const [productName, setProductName] = useState('');
-  const [productImage, setProductImage] = useState(null); // Use null for files
+
+  const initialImageState = {
+    image: null,
+    altText: "",
+  };
+  const [productName, setProductName] = useState("");
+  const [productImage, setProductImage] = useState([initialImageState]);
   const [videoFile, setVideoFile] = useState(null); // Use null for video files
 
   const initialFormData = {
@@ -27,12 +32,31 @@ const AddProduct = () => {
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    // Do something with the selected file, such as storing it in your form data state.
-    // For example, if you are using React and have a state variable named formData:
+
     setFormData({
       ...formData,
       product_images: file,
     });
+  };
+
+  const handleImageChange = (index, event) => {
+    const file = event.target.files[0];
+    const updatedImages = [...productImage];
+    updatedImages[index] = {
+      image: file,
+      altText: "",
+    };
+    setProductImage(updatedImages);
+  };
+
+  const addImageField = () => {
+    setProductImage([...productImage, initialImageState]);
+  };
+
+  const removeImageField = (index) => {
+    const updatedImages = [...productImage];
+    updatedImages.splice(index, 1);
+    setProductImage(updatedImages);
   };
 
   // Function to handle form submission
@@ -40,20 +64,14 @@ const AddProduct = () => {
     e.preventDefault();
 
     // Perform actions with the input values (e.g., send data to server)
-    console.log('Product Name:', productName);
-    console.log('Product Image:', productImage);
-    console.log('Video File:', videoFile);
+    console.log("Product Name:", productName);
+    console.log("Product Image:", productImage);
+    console.log("Video File:", videoFile);
 
     // Reset form fields
-    setProductName('');
-    setProductImage(null);
+    setProductName("");
+    setProductImage([initialImageState]);
     setVideoFile(null);
-  };
-
-  // Function to handle file input change for product image
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    setProductImage(file);
   };
 
   // Function to handle file input change for video file
@@ -63,161 +81,165 @@ const AddProduct = () => {
   };
 
   return (
-    <div className='Add-product'>
-    <div className='container'>
-      <h2 className='h2-tag'>Add Product</h2>
-      <form onSubmit={handleSubmit} className='form'>
-        
-      <Grid container spacing={3}>
-              <Grid item xs={6}>
-                <div className="field">
-                  <label className="lbl-field"> Product Name</label>
-                  <p>
-                    <input
-                      className="input-field"
-                      type="text"
-                      name="name"
-                      placeholder="Enter product name"
-                      value={formData.product_name}
-                      onChange={handleInputChange}
-                    />
-                  </p>
-                </div>
-              </Grid>
-              <Grid item xs={6}>
+    <div className="Add-product">
+      <div className="container">
+        <h2 className="h2-tag">Add Product</h2>
+        <form onSubmit={handleSubmit} className="form">
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
               <div className="field">
-  <label className="lbl-field">Product Image</label>
-  <p>
-    <input
-      className="input-field"
-      type="file"
-      name="product_images"
-      onChange={handleFileChange}
-    />
-  </p>
-</div>
-              </Grid>
-              <Grid item xs={6}>
-              <div className="field">
-  <label className="lbl-field">Video Url</label>
-  <p>
-    <input
-      className="input-field"
-      type="file"
-      name="product_images"
-      onChange={handleFileChange}
-    />
-  </p>
-</div>
-              </Grid>
-              <Grid item xs={6}>
-                <div className="field">
-                  <label className="lbl-field">Total Stocks</label>
-                  <p>
-                    <input
-                      className="input-field"
-                      type="text"
-                      name="stocks"
-                      placeholder="Enter total stocks"
-                      value={formData.total_stocks_added}
-                      onChange={handleInputChange}
-                    />
-                  </p>
-                </div>
-              </Grid>
-              <Grid item xs={6}>
-                <div className="field">
-                  <label className="lbl-field">No of Stocks</label>
-                  <p>
-                    <input
-                      className="input-field"
-                      type="text"
-                      name="stock"
-                      placeholder="Enter no of stocks"
-                      value={formData.no_of_stocks_available}
-                      onChange={handleInputChange}
-                    />
-                  </p>
-                </div>
-              </Grid>
-              <Grid item xs={6}>
-                <div className="field">
-                  <label className="lbl-field">Commission Rate</label>
-                  <p>
-                    <input
-                      className="input-field"
-                      type="text"
-                      name="rate"
-                      placeholder="Enter commission rate"
-                      value={formData.commission_rate}
-                      onChange={handleInputChange}
-                    />
-                  </p>
-                </div>
-              </Grid>
-              <Grid item xs={6}>
-                <div className="field">
-                  <label className="lbl-field"> MRP Price</label>
-                  <p>
-                    <input
-                      className="input-field"
-                      type="text"
-                      name="price"
-                      placeholder="Enter  price"
-                      value={formData.mrp_price}
-                      onChange={handleInputChange}
-                    />
-                  </p>
-                </div>
-              </Grid>
-
-              <Grid item xs={6}>
-                <div className="field">
-                  <label className="lbl-field">Offer</label>
-                  <p>
-                    <input
-                      className="input-field"
-                      type="text"
-                      name="offer"
-                      placeholder="Enter  Offer"
-                      value={formData.offer}
-                      onChange={handleInputChange}
-                    />
-                  </p>
-                </div>
-              </Grid>
-
-              <Grid item xs={6}>
-                <div className="field">
-                  <label className="lbl-field"> Final Price</label>
-                  <p>
-                    <input
-                      className="input-field"
-                      type="number"
-                      name="final rice"
-                      placeholder="Enter final price"
-                      value={formData.final_price}
-                      onChange={handleInputChange}
-                    />
-                  </p>
-                </div>
-              </Grid>
-
-              <Grid item xs={6}>
-                <div className="video-card">
-                 
-                </div>
-              </Grid>
-
-            
+                <label className="lbl-field"> Product Name</label>
+                <p>
+                  <input
+                    className="input-field"
+                    type="text"
+                    name="name"
+                    placeholder="Enter product name"
+                    value={formData.product_name}
+                    onChange={handleInputChange}
+                  />
+                </p>
+              </div>
             </Grid>
-        <button type="submit" className='submit-button'>Submit</button>
-      </form>
+            <Grid item xs={6}>
+              <div className="field">
+                <label className="lbl-field">Product Image</label>
+                {productImage.map((image, index) => (
+                  <div key={index}>
+                    <input
+                      type="file"
+                      onChange={(e) => handleImageChange(index, e)}
+                    />
+                    <button
+                      type="button"
+                      onClick={() => removeImageField(index)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                ))}
+                <button type="button" onClick={addImageField}>
+                  Add Image
+                </button>
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="field">
+                <label className="lbl-field">Video Url</label>
+                <p>
+                  <input
+                    className="input-field"
+                    type="file"
+                    name="product_images"
+                    onChange={handleFileChange}
+                  />
+                </p>
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="field">
+                <label className="lbl-field">Total Stocks</label>
+                <p>
+                  <input
+                    className="input-field"
+                    type="text"
+                    name="stocks"
+                    placeholder="Enter total stocks"
+                    value={formData.total_stocks_added}
+                    onChange={handleInputChange}
+                  />
+                </p>
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="field">
+                <label className="lbl-field">No of Stocks</label>
+                <p>
+                  <input
+                    className="input-field"
+                    type="text"
+                    name="stock"
+                    placeholder="Enter no of stocks"
+                    value={formData.no_of_stocks_available}
+                    onChange={handleInputChange}
+                  />
+                </p>
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="field">
+                <label className="lbl-field">Commission Rate</label>
+                <p>
+                  <input
+                    className="input-field"
+                    type="text"
+                    name="rate"
+                    placeholder="Enter commission rate"
+                    value={formData.commission_rate}
+                    onChange={handleInputChange}
+                  />
+                </p>
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="field">
+                <label className="lbl-field"> MRP Price</label>
+                <p>
+                  <input
+                    className="input-field"
+                    type="text"
+                    name="price"
+                    placeholder="Enter  price"
+                    value={formData.mrp_price}
+                    onChange={handleInputChange}
+                  />
+                </p>
+              </div>
+            </Grid>
+
+            <Grid item xs={6}>
+              <div className="field">
+                <label className="lbl-field">Offer</label>
+                <p>
+                  <input
+                    className="input-field"
+                    type="text"
+                    name="offer"
+                    placeholder="Enter  Offer"
+                    value={formData.offer}
+                    onChange={handleInputChange}
+                  />
+                </p>
+              </div>
+            </Grid>
+
+            <Grid item xs={6}>
+              <div className="field">
+                <label className="lbl-field"> Final Price</label>
+                <p>
+                  <input
+                    className="input-field"
+                    type="number"
+                    name="final rice"
+                    placeholder="Enter final price"
+                    value={formData.final_price}
+                    onChange={handleInputChange}
+                  />
+                </p>
+              </div>
+            </Grid>
+
+            <Grid item xs={6}>
+              <div className="video-card"></div>
+            </Grid>
+          </Grid>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
 };
 
 export default AddProduct;
-
-
